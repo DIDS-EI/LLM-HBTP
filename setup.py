6f4348a9c1d8dd59130b78a438609f3631a98614
DIDS-EI <DIDS_EI@163.com>
2024-06-11 15:06:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='btgym',
+    name='',
     version='0.1.0',
-    packages=['btgym'],
+    packages=[''],
     install_requires=required,
-    author='HPCL-EI',
-    author_email='hpcl_ei@163.com',
-    description='A Platform for Behavior Tree Generation and Evaluation',
-    url='https://github.com/HPCL-EI/BTGym',
+    author='',
+    author_email='',
+    description='',
+    url='',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
